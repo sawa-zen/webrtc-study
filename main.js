f4dbdd6d73445964ef87fe186109101ff1b90039
sawa-zen <sawadasuiren@gmail.com>
2024-01-25 01:54:35 +0900

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! remove room

--- main.js
+++ main.js
@@ -16,31 +16,16 @@ const io = new Server(server, {
 })
 
 io.on('connect', socket => {
-  console.log('io', 'connect')
-  console.log('io', 'socket: ', socket.id)
-
-  // サーバ側
-  socket.on('SEND_CALL', function() {
-    socket.broadcast.emit('RECEIVE_CALL', { id: socket.id });
-  })
-
   socket.on('SEND_OFFER', function(data) {
-    console.info('SEND_OFFER', data)
-    data.sdp.id = socket.id;
     socket.broadcast.emit('RECEIVE_OFFER', data.sdp)
   })
 
   socket.on('SEND_ANSWER', function(data) {
-    console.info('SEND_ANSWER', data)
-    data.sdp.id = socket.id;
-    socket.to(data.target).emit('RECEIVE_ANSWER', data.sdp)
+    socket.broadcast.emit('RECEIVE_ANSWER', data.sdp)
   })
 
-  // Ice Candidateを配信する
   socket.on('SEND_CANDIDATE', function(data) {
-    console.info('SEND_CANDIDATE', data)
-    data.ice.id = socket.id;
-    socket.to(data.target).emit('RECEIVE_CANDIDATE', data.ice)
+    socket.broadcast.emit('RECEIVE_CANDIDATE', data.ice)
   })
 })
 
