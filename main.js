be5e2af38ad12ce7ec8b6b25a7618aee1732b85e
SAWADA Takayoshi <sawadasuiren@gmail.com>
2024-03-06 01:45:48 +0900

    fixup! fix

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ app.use(express.static(__dirname + '/public'))
 
 const options = {
 	key: fs.readFileSync('./localhost-key.pem'),
-	cert: fs.readFileSync('./localhost.pem')
+	cert: fs.readFileSync('./localhost.pem'),
 };
 const server = https.createServer(options, app);
 
@@ -24,23 +24,30 @@ const io = new Server(server, {
 })
 
 io.on('connect', socket => {
+  console.info('connect')
+
   socket.on('disconnect', () => {
+    console.info('disconnect')
     socket.broadcast.emit('RECEIVE_DISCONNECT')
   })
 
   socket.on('SEND_CALL', function() {
+    console.info('send_call')
     socket.broadcast.emit('RECEIVE_CALL')
   })
 
   socket.on('SEND_OFFER', function(data) {
+    console.info('send_offer')
     socket.broadcast.emit('RECEIVE_OFFER', data.sdp)
   })
 
   socket.on('SEND_ANSWER', function(data) {
+    console.info('send_answer')
     socket.broadcast.emit('RECEIVE_ANSWER', data.sdp)
   })
 
   socket.on('SEND_CANDIDATE', function(data) {
+    console.info('send_candidate')
     socket.broadcast.emit('RECEIVE_CANDIDATE', data.ice)
   })
 })
